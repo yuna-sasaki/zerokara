fae7e14d311a5b10f8294b78b6a343d04d95b0ef
yuna-sasaki <nyu_ssk_stz@au.com>
2023-08-23 11:40:27 +0900

    Form Header Footer 追加

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="超高速凍結機、急速凍結機、液体凍結機、ナトリウム除去装置、凍結製造ライン設計の株式会社ゼロカラ">
+    <meta name="keywords" content="超高速凍結機,急速凍結機,液体凍結機,ナトリウム除去装置,凍結製造ライン設計">
+    <link rel="stylesheet" href="css/style.css">
+    <title>超高速凍結機、急速凍結機、液体凍結機、ナトリウム除去装置、凍結製造ライン設計の株式会社ゼロカラ</title>
+</head>
+
+<body>
+    <header>
+        <div class="header_inner">            
+            <a class="sp" href="#">
+                <img class="logo_sp" src="img/logo_sp.png" alt="">
+            </a>
+            <a class="pc" href="#">
+                <img class="logo_pc" src="img/logo_pc.png" alt="">
+            </a>
+            <a href="#contact">
+                <img class="cta_btn_head" src="img/cta_btn_head.png" alt="">
+            </a>
+        </div>
+    </header>
+
+    <section id="fv">
+        <img src="" alt="">
+    </section>
+
+    <section>
+        <img src="" alt="">
+    </section>
+
+    <section id="contact">
+        <div class="contact_inner">
+            <div class="sec_ttl">
+                <h2 class="helvetica">Contact</h2>
+                <h3 class="hiragino-mincho">お問い合わせ・資料請求</h3>
+            </div>
+            <p class="required notosans red">*必須</p>
+
+            <form action="mail.php" method="post">
+                <div class="contact_flex">
+                    <p class="notosans"><span class="red">*</span>会社名</p>
+                    <input type="text" name="会社名" required="">
+                </div>
+
+                <div class="contact_flex">
+                    <p class="notosans"><span class="red">*</span>氏名</p>
+                    <input type="text" name="氏名" required="">
+                </div>
+
+                <div class="contact_flex">
+                    <p class="notosans"><span class="red">*</span>フリガナ</p>
+                    <input type="text" name="フリガナ" required="">
+                </div>
+
+                <div class="contact_flex">
+                    <p class="notosans"><span class="red">*</span>メールアドレス</p>
+                    <input type="email" name="メールアドレス" required="">
+                </div>
+
+                <div class="contact_flex">
+                    <p class="notosans"><span class="red">*</span>確認用メールアドレス</p>
+                    <input type="email" name="確認用メールアドレス" required="">
+                </div>
+
+                <div class="contact_flex">
+                    <p class="notosans"><span class="red">*</span>電話番号</p>
+                    <input type="text" name="電話番号" required="">
+                </div>
+
+                <div class="contact_flex select">
+                    <p class="notosans"><span class="red">*</span>お問い合わせ内容</p>
+                    <select name="お問い合わせ内容" required="">
+                        <option></option>
+                        <option value="凍結機や機材等に関する内容">凍結機や機材等に関する内容</option>
+                        <option value="凍結デモに関する内容">凍結デモに関する内容</option>
+                        <option value="試食会に関する内">試食会に関する内容</option>
+                        <option value="資料請求">資料請求</option>
+                        <option value="その他のご質問">その他のご質問</option>
+                    </select>
+                </div>
+
+                <div class="contact_flex">
+                    <p class="notosans">詳細</p>
+                    <textarea name="詳細" id="" cols="30" rows="10"></textarea>
+                </div>
+
+                <div class="contact_flex">
+                    <p class="notosans"><span class="red">*</span>個人情報保護方針</p>
+                    <div class="privacypolicy notosans">
+                        株式会社ゼロカラ（以下、弊社）は、以下の通り個人情報の取り扱いに関する方針を定め、ご提供いただきました個人情報の保護に努めてまいります。<br>
+                        <p></p>
+                        <p>個人情報とは<br><br>
+                            弊社が利用者の方々から提供を受けた住所、氏名、年齢、性別、電話番号、E-mailアドレス等、利用者ご本人を識別できる情報、および利用者ご本人固有の情報を意味します。<br></p>
+                        <p>個人情報の収集について<br><br>
+                            弊社がインターネットを通じて個人情報を収集する際は、収集目的をあらかじめ明示し、利用者ご本人の意思による情報の提供を原則といたします。<br></p>
+                        <p>個人情報の機密保持について<br><br>
+                            個人情報を利用者ご本人の同意なくして収集目的以外に利用することはありません。ただし、以下の場合は個人情報を開示することがあります。<br><br>
+                            １）ご本人の同意がある場合。<br><br>
+                            ２）法令等により裁判所・警察機関などの公的機関から提供を求められた場合。<br></p>
+                        <p>個人情報の管理について<br><br>
+                            ご提供いただきました個人情報につきましては、漏洩、流用等を防止するため厳重な管理に努め、安心してご利用いただけるよう努力してまいります。<br><br>
+                            また、個人情報の保護を図るためや、法令その他の規範の変更に伴い、プライバシーポリシーを改定することがあります。<br><br>
+                            その場合は、ホームページにてお知らせいたします。<br></p>
+                        <p>【個人情報管理責任者とお問合せ先】<br><br>
+                            神奈川県横浜市港北区新吉田東3-31-10<br>
+                            株式会社ゼロカラ<br></p>
+                        <p>お問い合わせ：メールフォーム
+                        </p>
+                    </div>
+                </div>
+
+                <div class="privacypolicy_flex">
+                    <label>
+                        <input type="checkbox" id="checkbox01" required="">
+                        <span class="notosans">個人情報保護方針に同意</span>
+                    </label>
+                </div>
+                <button type="submit" class="submit_btn" value="send"><img src="img/submit_btn.png" alt=""></button>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <p class="notosans copyright">© ZEROKARA CO.,LTD. All rights reserved.</p>
+    </footer>
+
+</body>
+
+</html>
